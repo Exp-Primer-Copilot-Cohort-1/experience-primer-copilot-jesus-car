0307cbfa2c11dc1d2b42eb2abc2517f8f5f771fb
jesus-car <jesus.garcia2611@hotmail.com>
2024-05-10 16:51:32 -0500

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        greet: function() {
+            console.log(`Hello, my name is ${this.name} and I'm ${this.age}.`);
+        },
+        displaySkills: function() {
+            this.skills.forEach(skill => console.log(skill));
+        }
+    };
+    return member;
+}
